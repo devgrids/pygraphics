09ab8426f50ef138cb6cbd6c411c5799a0290406
devgrids <devgrids@gmail.com>
2023-12-30 12:51:50 +0100

    add test

--- deep/test.py
+++ deep/test.py
@@ -1,53 +1,16 @@
-import glfw
-from OpenGL.GL import *
-import numpy as np
+from PIL import Image, ImageDraw, ImageFont
 
-def draw_guizmo():
-    # Ejes X, Y, Z
-    glBegin(GL_LINES)
-    
-    # Eje X en rojo
-    glColor3f(1, 0, 0)
-    glVertex3f(0, 0, 0)
-    glVertex3f(1, 0, 0)
+# Crear una nueva imagen con fondo blanco
+image = Image.new('RGB', (200, 100), color = (255, 255, 255))
 
-    # Eje Y en verde
-    glColor3f(0, 1, 0)
-    glVertex3f(0, 0, 0)
-    glVertex3f(0, 1, 0)
+# Crear un objeto para dibujar en la imagen
+draw = ImageDraw.Draw(image)
 
-    # Eje Z en azul
-    glColor3f(0, 0, 1)
-    glVertex3f(0, 0, 0)
-    glVertex3f(0, 0, 1)
+# Cargar una fuente (se necesita el archivo de la fuente)
+font = ImageFont.truetype("deep/norwester.otf", 16)
 
-    glEnd()
+# Dibujar texto
+draw.text((10, 10), "Hola, mundo!", fill=(0, 0, 0), font=font)
 
-def main():
-    if not glfw.init():
-        return
-
-    window = glfw.create_window(800, 600, "3D Guizmo Example", None, None)
-    if not window:
-        glfw.terminate()
-        return
-
-    glfw.make_context_current(window)
-    glClearColor(0.0, 0.0, 0.0, 1.0)
-    glEnable(GL_DEPTH_TEST)
-
-    while not glfw.window_should_close(window):
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-
-        # Aquí ajustas la cámara/proyección si lo necesitas
-        # ...
-
-        draw_guizmo()
-
-        glfw.swap_buffers(window)
-        glfw.poll_events()
-
-    glfw.terminate()
-
-if __name__ == "__main__":
-    main()
+# Guardar la imagen
+image.save('deep/texto.png')
